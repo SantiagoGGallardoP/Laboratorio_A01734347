b574f6a27ab68656c4334f67467bb09b0ec3a5d2
SantiagoGGallardoP <131408202+SantiagoGGallardoP@users.noreply.github.com>
2023-06-05 19:31:00 -0600

    Update actividadintegradora_a01734347.py

--- actividadintegradora_a01734347.py
+++ actividadintegradora_a01734347.py
@@ -10,12 +10,16 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 
-st.title("Police Incident Reports from 2018 to 2020 ins San Francisco")
+st.title("Police Incident Reports from 2018 to 2020 in San Francisco")
 
-df= pd.read_csv("https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=drive_link")
+df = pd.read_csv("https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=drive_link")
 
 st.markdown("The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.")
 
-mapa = df[df["City"] == "San Francisco"].copy()
-mapa=mapa.dropna()
-st.map(mapa.astype(int))
+# Verificar si las columnas 'Latitude' y 'Longitude' existen en el DataFrame
+if 'Latitude' in df.columns and 'Longitude' in df.columns:
+    mapa = df[['Latitude', 'Longitude']].dropna()
+    st.map(mapa)
+else:
+    st.write("Las columnas 'Latitude' y 'Longitude' no se encuentran en el DataFrame.")
+
